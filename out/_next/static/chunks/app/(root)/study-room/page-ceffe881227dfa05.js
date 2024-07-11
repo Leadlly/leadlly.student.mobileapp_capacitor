8f6b22fa94b8328ea03ec40efe3ffc489fbcb850
adil-khursheed <adilkhursheed60@gmail.com>
2024-07-11 16:57:00 +0530

    out directory

---
+++ out/_next/static/chunks/app/(root)/study-room/page-ceffe881227dfa05.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[651,345,875,477,235,29,695,146],{3521:function(){}},function(n){n.O(0,[971,23,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
